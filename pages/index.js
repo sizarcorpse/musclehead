a37e9c99a66d89228f22232b1c14e6af52cae1a2
Sizar Corpse <sizarcorpse@gmail.com>
2022-08-19 08:42:05 +0600

    create page home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
-const Home = () => {
-  return <>MuscleHead</>;
+import { Container, Grid, styled } from "@mui/material";
+import { mockHome } from "mocks/";
+import Head from "next/head";
+
+const Home = (props) => {
+  const {} = props;
+  const { hero } = mockHome;
+  return (
+    <Grid>
+      <Head>
+        <title>Home</title>
+        <meta name="description" content="#" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Grid item></Grid>
+    </Grid>
+  );
 };
 
 export default Home;
