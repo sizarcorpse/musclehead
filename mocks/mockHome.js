3bce9a4f0dc4818d684de9f2c5db939c98dc489e
Sizar Corpse <sizarcorpse@gmail.com>
2022-08-19 07:59:33 +0600

    update mocks mockHome

--- mocks/mockHome.js
+++ mocks/mockHome.js
@@ -3,10 +3,10 @@ const mockHome = {
     slides: [
       {
         photo: {
-          url: "/assets/jpg/home-slide-1.1.jpg",
+          url: "/assets/jpg/home-slide-1.jpg",
           alt: "",
         },
-        title: "Strength, fitness and athleticism. Sustained",
+        title: "Strength, fitness and athleticism and Sustained.",
         subtitle: "",
         description: "",
         buttons: [
@@ -18,12 +18,44 @@ const mockHome = {
       },
       {
         photo: {
-          url: "",
+          url: "/assets/jpg/home-slide-2.jpg",
           alt: "",
         },
-        title: "Strength, fitness and athleticism. Sustained",
+        title: "Holistic good health.",
         subtitle: "",
         description: "",
+        buttons: [
+          {
+            text: "Learn More",
+            link: "/about",
+          },
+        ],
+      },
+      {
+        photo: {
+          url: "/assets/jpg/home-slide-3.jpg",
+          alt: "",
+        },
+        title: "A healthy lifestyle.",
+        subtitle: "your health and fitness",
+        description:
+          "Getting to know absolutely everything about you – the full picture of your health and fitness. This free consultation will happen over FaceTime and will be followed up with a questionnaire and movement screen.",
+        buttons: [
+          {
+            text: "Learn More",
+            link: "/about",
+          },
+        ],
+      },
+      {
+        photo: {
+          url: "/assets/jpg/home-slide-4.jpg",
+          alt: "",
+        },
+        title: "Customized Training Program.",
+        subtitle: "Staying on track",
+        description:
+          "Your program will be built specifically for you. You'll achieve your performance and aesthetic goals, without living in the gym.",
         buttons: [
           {
             text: "Learn More",
